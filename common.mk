4551c97de8fe0e4790757d588ea7fbd542a41962
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sm6375-common: Build dummy libqti-perfd-client
    Change-Id: I796e7c1da7eb442d60de7a0183205c049d89149e

--- common.mk
+++ common.mk
@@ -270,7 +270,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # Properties
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
@@ -290,7 +291,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/motorola
+    hardware/motorola \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \

fe9bafc7d8803b89e0c9a788e8f9bb41aa330735
Vivekachooz <vivekachooz@gmail.com>
2023-11-24 19:08:51 +0530

    sm6375-common: Import and adapt powerhint from redfin
    * From redfin-user-13-TQ3A.230901.001-10750268-release-keys
    
    Change-Id: I923c1adf70d035c9def0032ccf29530ce92ebe66

--- common.mk
+++ common.mk
@@ -273,6 +273,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
     ro.carrier

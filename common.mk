df75d5ee494e44c895d788988a46a92f52ce5cc2
Vivekachooz <vivekachooz@gmail.com>
2024-07-18 20:16:00 +0530

    sm6375-common: Import and adapt powerhint from redfin
    * From redfin-user-13-TQ3A.230901.001-10750268-release-keys
    
    Change-Id: I923c1adf70d035c9def0032ccf29530ce92ebe66

--- common.mk
+++ common.mk
@@ -393,6 +393,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.moto_sm6375-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \

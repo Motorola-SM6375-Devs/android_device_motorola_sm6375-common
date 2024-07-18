1e25d6fd854af892732535e0f0e8844a9a155291
Lucchetto <lucchetto.tie@live.com>
2024-07-18 20:16:00 +0530

    sm6375-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx.vendor \
     libstagefright_softomx_plugin.vendor
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \

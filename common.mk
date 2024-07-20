928b8b38b5a989f8baa4ed0498e7543a0102ce4d
Anand S <anandzzz360@gmail.com>
2024-07-20 20:16:00 +0530

    sm6375-common: Switch to GNSS HAL from stock
    * From U1UG34M.23-82-2
    
    Change-Id: I75ac90f482127ae1119e2c691d11b2b574f1efb4

--- common.mk
+++ common.mk
@@ -207,6 +207,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \

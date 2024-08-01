cb6fe0ac8cee6778b4c927240ce0e050a40cf593
johnmart19 <johnivan19999@gmail.com>
2024-08-01 12:56:42 +0530

    sm6375-common: Build android.hardware.media.omx from source
    * Fixes: android.hardware.media.omx@1.0-service: Service not available yet.
    
    Change-Id: I9581b6d244f4336d2cff4785ba99bd64079faa1f

--- common.mk
+++ common.mk
@@ -287,6 +287,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
+    libstagefright_omx.vendor \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor

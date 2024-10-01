7962f0faab54b45c3e742804070253391b89ef43
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-01 20:16:00 +0530

    sm6375-common: automatically patch ims
    https://review.lineageos.org/q/topic:%22extract-utils-apktool%22
    
    Change-Id: I704e191adc7aafe6fe062c1bb330fbd1ca05493c

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,9 @@ function blob_fixup() {
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
+        system_ext/priv-app/ims/ims.apk)
+            apktool_patch "${2}" "$MY_DIR/ims-patches"
+            ;;
         vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
             sed -ni '/default.*fqname/!p' "${2}"
             ;;

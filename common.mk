f125b49b3b06413f5038789151fc04a0a555562d
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:57:40 -0400

    sm6375-common: Ensure we build the vendor version of libcrypto_shim
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: I9c9340b37a1ad0d6c22d3947541fe1196a5efeec

--- common.mk
+++ common.mk
@@ -193,7 +193,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim
+    libcrypto_shim.vendor
 
 # fastbootd
 PRODUCT_PACKAGES += \

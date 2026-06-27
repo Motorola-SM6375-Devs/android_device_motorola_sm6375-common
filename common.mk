f082ef714f276203452b73111115832b0363a175
Anand S <anandzzz360@gmail.com>
2026-06-27 20:09:12 +0530

    sm6375-common: Decommonise charging control path
    Change-Id: I2b632cf7fe8554c685ad96a95c71c179b9bd24df

--- common.mk
+++ common.mk
@@ -201,11 +201,6 @@ $(call soong_config_set_bool,livedisplay_sysfs,enable_se,true)
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,sys/class/power_supply/battery/input_current_limit)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/blair/media_codecs_performance_blair.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_blair.xml \
